e18a6816b7e2f6dba41bb9e5b70156ecb709053e
tokuyama shouta <toku1031ts@gmail.com>
2021-10-20 20:47:06 +0900

    axios2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import Counter from './components/Counter'
+import React,{useState} from 'react'
+import jsonplaceholder from './apis/jsonplaceholder';
+import Button from './components/Button';
+import Resources from './components/Resources';
 
 const App = () => {
+  const [resources, setResources] = useState([]);
+
+  const getPosts=async()=>{
+    try{
+      const posts = await jsonplaceholder.get('/posts');
+      setResources(posts.data); 
+    }catch(err){
+      console.log(err);
+      
+    }
+  }
+
+  const getAlbums=async()=>{
+    try{
+      const albums = await jsonplaceholder.get('/albums');
+      setResources(albums.data); 
+    }catch(err){
+      console.log(err);
+      
+    }
+  }
+
   return (
-    <div className='ui container' style={{marginTop:'20px'}}>
-      <Counter/>
+    <div className="ui container" style={{marginTop: '30px'}}>
+      <Button onClick={getPosts} color='primary' text='posts'/>
+      <Button onClick={getAlbums} color='red' text='album'/>
+      <Resources resources={resources}/>
     </div>
   )
 }
---
+++ src/components/Resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resources = ({resources}) => {
+  return(
+    <>
+      {resources.map((resource) =>(
+        <p key={resource.id}>{resource.title}</p>
+      ))}
+    </>
+  )
+}
+
+export default Resources;
