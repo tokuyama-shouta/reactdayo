593ba5b700eea47ef1bb3ff66582bb5dfccb78a8
tokuyama shouta <toku1031ts@gmail.com>
2021-10-21 21:49:30 +0900

    画像検索アプリ1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import React,{useState} from 'react'
-import jsonplaceholder from './apis/jsonplaceholder';
-import Button from './components/Button';
-import Resources from './components/Resources';
+import React from 'react';
+import SearchBar from './components/SearchBar';
 
-const App = () => {
-  const [resources, setResources] = useState([]);
-
-  const getPosts=async()=>{
-    try{
-      const posts = await jsonplaceholder.get('/posts');
-      setResources(posts.data); 
-    }catch(err){
-      console.log(err);
-      
-    }
+const App=()=> {
+  const onSearchSubmit=(term)=>{
+    console.log(term);
   }
-
-  const getAlbums=async()=>{
-    try{
-      const albums = await jsonplaceholder.get('/albums');
-      setResources(albums.data); 
-    }catch(err){
-      console.log(err);
-      
-    }
-  }
-
-  return (
-    <div className="ui container" style={{marginTop: '30px'}}>
-      <Button onClick={getPosts} color='primary' text='posts'/>
-      <Button onClick={getAlbums} color='red' text='album'/>
-      <Resources resources={resources}/>
+  return(
+    <div className='ui container' style={{marginTop:'20px'}}>
+      <SearchBar onSubmit={onSearchSubmit}/>
     </div>
   )
 }
