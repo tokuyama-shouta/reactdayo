fb0b5925e41996b4271d1285cbbbade3578d5627
tokuyama shouta <toku1031ts@gmail.com>
2021-10-22 20:23:06 +0900

    画像検索アプリ2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios'
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 
 const App=()=> {
-  const onSearchSubmit=(term)=>{
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit= async(term)=>{
+    try{
+      const params={
+        key:"18403602-3bc7efb75c96821e59b30df03",
+        q : term,
+      };
+      const responce = await axios.get("https://pixabay.com/api/",{params});
+      setImages(responce.data.hits);
+      if(responce.data.total===0){
+        window.alert('お探しの画像はありません。')
+      }
+    }catch{
+       window.alert('写真の取得に失敗しました。')
+    }
   }
   return(
     <div className='ui container' style={{marginTop:'20px'}}>
       <SearchBar onSubmit={onSearchSubmit}/>
+      <ImageList images={images}/>
     </div>
   )
 }
 
 export default App
+
+//18403602-3bc7efb75c96821e59b30df03
