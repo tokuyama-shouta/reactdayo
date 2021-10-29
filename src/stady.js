d0b7ff4c3a5b1c5ae4c8acbffc0d24f853bf7ac4
tokuyama shouta <toku1031ts@gmail.com>
2021-10-29 21:53:18 +0900

    変数

---
+++ src/stady.js
@@ -0,0 +1,46 @@
+// const let
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+// //letは上書き可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言"
+
+// const val3 = "const変数";
+// console.log(val3);
+// //const変数は上書きふか
+// val3 = "const変数を上書き";
+// const val3 = "constを再宣言"
+
+//constで定義したオブジェクトはプロパティの変更可能
+// const val4 = {
+//   name: 'pkpk',
+//   age: 26
+// };
+// val4.name = 'pkpkdayo';
+// val4.addres = "pk"
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更可能
+// const val5 = [1,2]
+// val5[0] = 4;
+// val5.push(6)
+// console.log(val5);
+
+
+
+
+
+
+
