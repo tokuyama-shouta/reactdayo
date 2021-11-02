461f8071a00870105c49663042023a832ec89a3f
tokuyama shouta <toku1031ts@gmail.com>
2021-11-02 19:47:51 +0900

    デフォルト値

--- src/stady.js
+++ src/stady.js
@@ -64,25 +64,30 @@
 // console.log(func3(10,20));
 
 //分割代入
-const myProfile = {
-  name: "pkpk",
-  age: 26,
-}
+// const myProfile = {
+//   name: "pkpk",
+//   age: 26,
+// }
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
 
-const { name, age} = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const { name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
 
-const myProfile = ['pkpk',26];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-console.log(message3);
+// const myProfile = ['pkpk',26];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
 
-const [name,age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message4);
+// const [name,age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+//デフォルト値
+const sayHello = (name) => console.log(`こんにちは！${name}さん`);
+sayHello('pkpk');
+
 
 
 
