45559c43b93b80939b7a3f19d415498c6c08a034
tokuyama shouta <toku1031ts@gmail.com>
2021-11-02 19:45:15 +0900

    分割代入

--- src/stady.js
+++ src/stady.js
@@ -50,18 +50,42 @@
 //アロー関数
 
 //従来の関数
-function func1(str) {
-  return str;
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です。"));
+
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1,num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10,20));
+
+//分割代入
+const myProfile = {
+  name: "pkpk",
+  age: 26,
 }
-console.log(func1("func1です。"));
 
-const func2 = (str) => str;
-console.log(func2("func2です。"));
+const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+console.log(message1);
 
-const func3 = (num1,num2) => {
-  return num1 + num2;
-};
-console.log(func3(10,20));
+const { name, age} = myProfile;
+const message2 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message2);
+
+const myProfile = ['pkpk',26];
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+
+const [name,age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です。`;
+console.log(message4);
+
+
+
 
 
 
